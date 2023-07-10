847237a92243fbaff6762d02b99c4e48900a0472
ante <49555217+ante185@users.noreply.github.com>
2023-07-10 03:48:44 +0200

    prep for repaint

---
+++ A3A/addons/config_fixes/CfgVehicles/vanilla_black_vehicles.hpp
@@ -0,0 +1,73 @@
+class O_LSV_02_AT_F;	
+class B_LSV_01_AT_F;	
+class I_C_Offroad_02_unarmed_F;	
+class I_C_Offroad_02_AT_F;	
+class I_C_Offroad_02_LMG_F;	
+class O_MBT_04_cannon_F;	
+class O_MBT_04_command_F;	
+class I_C_Heli_Light_01_civil_F;	
+class O_Truck_02_Ammo_F;	
+class O_Truck_02_Fuel_F;	
+class O_Truck_02_box_F;	
+class O_Truck_02_transport_F;	
+class O_Truck_02_covered_F;	
+class O_Truck_02_medical_F;	
+
+//these are so far all the vanilla vehicles that have an alternative black texture, that i've found
+
+class a3a_ION_LSV_02_AT_F	:	O_LSV_02_AT_F
+{
+
+};
+class a3a_ION_LSV_01_AT_F	:	B_LSV_01_AT_F
+{
+
+};
+class a3a_ION_Offroad_02_unarmed_F	:	I_C_Offroad_02_unarmed_F
+{
+
+};
+class a3a_ION_Offroad_02_AT_F	:	I_C_Offroad_02_AT_F
+{
+
+};
+class a3a_ION_Offroad_02_LMG_F	:	I_C_Offroad_02_LMG_F
+{
+
+};
+class a3a_ION_MBT_04_cannon_F	:	O_MBT_04_cannon_F
+{
+
+};
+class a3a_ION_MBT_04_command_F	:	O_MBT_04_command_F
+{
+
+};
+class a3a_ION_Heli_Light_01_civil_F	:	I_C_Heli_Light_01_civil_F
+{
+
+};
+class a3a_ION_Truck_02_Ammo_F	:	O_Truck_02_Ammo_F
+{
+
+};
+class a3a_ION_Truck_02_Fuel_F	:	O_Truck_02_Fuel_F
+{
+
+};
+class a3a_ION_Truck_02_box_F	:	O_Truck_02_box_F
+{
+
+};
+class a3a_ION_Truck_02_transport_F	:	O_Truck_02_transport_F
+{
+
+};
+class a3a_ION_Truck_02_covered_F	:	O_Truck_02_covered_F
+{
+
+};
+class a3a_ION_Truck_02_medical_F	:	O_Truck_02_medical_F
+{
+
+};
