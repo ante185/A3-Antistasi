6e7461e9c01b9bf648a2e21270d6e97b21dd8083
ante <49555217+ante185@users.noreply.github.com>
2026-06-07 22:25:56 +0200

    added the auto cannon tank from BF1

--- A3A/addons/config_fixes/WW1/config.cpp
+++ A3A/addons/config_fixes/WW1/config.cpp
@@ -17,6 +17,15 @@ class CfgPatches
         VERSION_CONFIG;
     };
 };
+class cfgAmmo {
+    class LIB_B_20x110_AP;
+    class a3a_LIB_20mmMixed : LIB_B_20x110_AP{
+        submunitionAmmo[] = {"LIB_B_20x110_AP",0.5,"LIB_B_20x110_AA",0.5};
+        submunitionConeAngle = 0;
+        submunitionConeAngleHorizontal = 0;
+        triggerTime = 0.0001;
+    };
+};
 
 class CfgMagazines {
     class WWI_9mm_32xMP18;
@@ -26,12 +35,26 @@ class CfgMagazines {
         count = 5;
         displayName = "Berthier 5 round clip";
     };
+    class LIB_60x_OerlikonAP;
+    class a3a_LIB_10x_20mmMixed : LIB_60x_OerlikonAP{
+        ammo = "a3a_LIB_20mmMixed";
+        count = 11;
+        displayName = "20mm HE/AP Mix";
+        maxLeadSpeed = 60;
+        tracersEvery=1;
+    };
 };
 
 class CfgWeapons {
     class Rifle_Base_F;
     class wwi_berthier : Rifle_Base_F{
         magazines[] = {"a3a_WWI_5Rnd_Berthier"};
+    };
+    class LIB_Oerlikon_1;
+    class ww1_20mm_autocannon : LIB_Oerlikon_1 {
+        displayName = "20mm Mle 1917";
+        initSpeed = -0.3; //30% initial speed, very high arcing rounf
+        magazines[] = {"a3a_LIB_10x_20mmMixed"};
     };
 };
 
@@ -56,7 +79,9 @@ class CfgVehicles {
         tankTurnForce = 225000;
         terrainCoef = 1;
     };
-    class wwi_ft17_2018 : wwi_ft17 {};
+    class wwi_ft17_2018 : wwi_ft17 {
+        class Turrets;
+    };
     class wwi_schneider_ca1 : wwi_ft17_2018 {
         class complexGearbox : complexGearbox {
             GearboxRatios[] = {"R2",-14,"N",0,"D1",4,"D2",3.1,"D3",2.6,"D4",1.47,"D5",1.33};
@@ -87,5 +112,20 @@ class CfgVehicles {
         peakTorque = 6254;
         tankTurnForce = 225000;
         terrainCoef = 1;
+    };
+
+    class wwi_ft17_8mm_girod : wwi_ft17_2018 {
+        class Turrets : Turrets {
+            class MainTurret;
+        };
+    };
+    class a3a_wwi_ft17_20mm : wwi_ft17_8mm_girod {
+        displayName = "FT17 20mm";
+        class Turrets : Turrets {
+            class MainTurret : MainTurret{
+                magazines[] = {"a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed","a3a_LIB_10x_20mmMixed"};
+                weapons[] = {"ww1_20mm_autocannon"};
+            };
+        };
     };
 };
